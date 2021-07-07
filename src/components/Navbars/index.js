2876d682cf51f430e3c2c095f688ce88bee008b6
nhatlam1995 <nhatlam1695@gmail.com>
2021-07-07 20:34:22 +0700

    Header

--- src/components/Navbars/index.js
+++ src/components/Navbars/index.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Col,
+} from 'reactstrap';
+import addImage from '../../assets/images/add1.png'
+import './index.css'
 
-function Navbars() {
+const Navbars = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
-        <div>
-            Navbars
-        </div>
+        <Navbar light expand="md">
+            <NavbarBrand href="/" >
+                <div className='d-flex align-items-center'>
+                    <div className='col-sm'>
+                        <img
+                            src={addImage}
+                            alt='icon' />
+                    </div>
+                    Khai Báo Y Tế
+                </div>
+            </NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                    <Col xs="auto">
+                        <NavItem>
+                            <NavLink href="/">Đăng kí</NavLink>
+                        </NavItem>
+                    </Col>
+                    <Col xs="auto">
+                        <NavItem>
+                            <NavLink href="/dashboard">Dashboard</NavLink>
+                        </NavItem>
+                    </Col>
+                </Nav>
+            </Collapse>
+        </Navbar>
     )
-}
+};
 
 export default Navbars
