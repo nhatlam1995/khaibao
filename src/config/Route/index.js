f403995e617ce015b50c0e0bc9b970310568f879
nhatlam1995 <nhatlam1695@gmail.com>
2021-07-07 20:02:06 +0700

    Base
    Base

---
+++ src/config/Route/index.js
@@ -0,0 +1,15 @@
+export const routeConfig = {
+    "login": {
+        "list-url": "/login"
+    },
+    "register":{
+        "list-url": "/register"
+    },
+    "form": {
+        "list-url": "/form"
+    },
+    "report": {
+        "list-url": "/report"
+    }
+}
+export default routeConfig
